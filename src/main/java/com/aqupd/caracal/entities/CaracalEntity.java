5803dd0adb7a501badf1883080a65872a2fb66b0
AqUpd <47220198+AquUpd@users.noreply.github.com>
2021-05-25 17:22:23 +0300

    Анимация сидения и приручение!

--- src/main/java/com/aqupd/caracal/entities/CaracalEntity.java
+++ src/main/java/com/aqupd/caracal/entities/CaracalEntity.java
@@ -1,7 +1,7 @@
 package com.aqupd.caracal.entities;
 
-import com.aqupd.caracal.Main;
 import com.aqupd.caracal.aqupdSoundEvents;
+import com.google.common.collect.Maps;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.entity.*;
@@ -9,21 +9,27 @@ import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.passive.BatEntity;
-import net.minecraft.entity.passive.ChickenEntity;
-import net.minecraft.entity.passive.ParrotEntity;
-import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.entity.passive.*;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.sound.SoundCategory;
+import net.minecraft.item.DyeItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.*;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.*;
 import org.jetbrains.annotations.Nullable;
 
-public class CaracalEntity extends MobEntity {
+import java.util.Map;
 
+public class CaracalEntity extends TameableEntity {
+    private static final Ingredient TAMING_INGREDIENT;
 
     public CaracalEntity(EntityType<? extends CaracalEntity> entityType, World world) {
         super(entityType, world);
@@ -31,11 +37,14 @@ public class CaracalEntity extends MobEntity {
     
     protected void initGoals() {
         this.goalSelector.add(1, new SwimGoal(this));
-        this.goalSelector.add(2, new PounceAtTargetGoal(this, 0.3F));
-        this.goalSelector.add(3, new AttackGoal(this));
-        this.goalSelector.add(4, new LookAtEntityGoal(this, PlayerEntity.class, 10.0F));
+        this.goalSelector.add(1, new SitGoal(this));
+        //this.goalSelector.add(2, new FollowOwnerGoal());
+        this.goalSelector.add(5, new PounceAtTargetGoal(this, 0.3F));
+        this.goalSelector.add(6, new AttackGoal(this));
+        this.goalSelector.add(7, new LookAtEntityGoal(this, PlayerEntity.class, 10.0F));
+        this.goalSelector.add(8, new WanderAroundGoal(this, 0.5F));
         this.targetSelector.add(1, new FollowTargetGoal<>(this, ChickenEntity.class, false));
-        this.targetSelector.add(2, new FollowTargetGoal<>(this, ParrotEntity.class, false));
+        this.targetSelector.add(2, new FollowTargetGoal<>(this, RabbitEntity.class, false));
         this.targetSelector.add(3, new FollowTargetGoal<>(this, BatEntity.class, false));
     }
 
@@ -67,15 +76,11 @@ public class CaracalEntity extends MobEntity {
                 .add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, 0.5);
     }
 
-    public boolean canImmediatelyDespawn(double distanceSquared) {
-        return this.age > 2400;
-    }
-
     public boolean handleFallDamage(float fallDistance, float damageMultiplier) {
         return false;
     }
 
-    @Override
+    @Nullable
     protected SoundEvent getAmbientSound() {
         return aqupdSoundEvents.CARACAL_AMBIENT;
     }
@@ -91,8 +96,120 @@ public class CaracalEntity extends MobEntity {
         return SoundEvents.ENTITY_OCELOT_DEATH;
     }
 
+    private float getAttackDamage() {
+        return (float)this.getAttributeValue(EntityAttributes.GENERIC_ATTACK_DAMAGE);
+    }
+
+    public boolean tryAttack(Entity target) {
+        return target.damage(DamageSource.mob(this), this.getAttackDamage());
+    }
+
+
+    protected void eat(PlayerEntity player, ItemStack stack) {
+        if (this.isBreedingItem(stack)) {
+            this.playSound(SoundEvents.ENTITY_CAT_EAT, 1.0F, 1.0F);
+        }
+
+        super.eat(player, stack);
+    }
+/*
+    public CaracalEntity createChild(ServerWorld serverWorld, PassiveEntity passiveEntity) {
+        CaracalEntity caracalEntity = (CaracalEntity)EntityType.CARACAL.create(serverWorld);
+        if (passiveEntity instanceof CaracalEntity) {
+            if (this.isTamed()) {
+                caracalEntity.setOwnerUuid(this.getOwnerUuid());
+                caracalEntity.setTamed(true);
+            }
+        }
+        return caracalEntity;
+    }
+*/
+    public boolean canBreedWith(AnimalEntity other) {
+        if (!this.isTamed()) {
+            return false;
+        } else if (!(other instanceof CaracalEntity)) {
+            return false;
+        } else {
+            CaracalEntity caracalEntity = (CaracalEntity)other;
+            return caracalEntity.isTamed() && super.canBreedWith(other);
+        }
+    }
+
+    public ActionResult interactMob(PlayerEntity player, Hand hand) {
+        ItemStack itemStack = player.getStackInHand(hand);
+        Item item = itemStack.getItem();
+        if (this.world.isClient) {
+            if (this.isTamed() && this.isOwner(player)) {
+                return ActionResult.SUCCESS;
+            } else {
+                return !this.isBreedingItem(itemStack) || !(this.getHealth() < this.getMaxHealth()) && this.isTamed() ? ActionResult.PASS : ActionResult.SUCCESS;
+            }
+        } else {
+            ActionResult actionResult;
+            if (this.isTamed()) {
+                if (this.isOwner(player)) {
+                    if (!(item instanceof DyeItem)) {
+                        if (item.isFood() && this.isBreedingItem(itemStack) && this.getHealth() < this.getMaxHealth()) {
+                            this.eat(player, itemStack);
+                            this.heal((float)item.getFoodComponent().getHunger());
+                            return ActionResult.CONSUME;
+                        }
+
+                        actionResult = super.interactMob(player, hand);
+                        if (!actionResult.isAccepted() || this.isBaby()) {
+                            this.setSitting(!this.isSitting());
+                        }
+
+                        return actionResult;
+                    }
+                }
+            } else if (this.isBreedingItem(itemStack)) {
+                this.eat(player, itemStack);
+                if (this.random.nextInt(3) == 0) {
+                    this.setOwner(player);
+                    this.setSitting(true);
+                    this.world.sendEntityStatus(this, (byte)7);
+                } else {
+                    this.world.sendEntityStatus(this, (byte)6);
+                }
+
+                this.setPersistent();
+                return ActionResult.CONSUME;
+            }
+
+            actionResult = super.interactMob(player, hand);
+            if (actionResult.isAccepted()) {
+                this.setPersistent();
+            }
+
+            return actionResult;
+        }
+    }
+
+    public boolean isBreedingItem(ItemStack stack) {
+        return TAMING_INGREDIENT.test(stack);
+    }
+
+    protected float getActiveEyeHeight(EntityPose pose, EntityDimensions dimensions) {
+        return dimensions.height * 0.5F;
+    }
+
+    public boolean canImmediatelyDespawn(double distanceSquared) {
+        return !this.isTamed() && this.age > 2400;
+    }
+
+    static {
+        TAMING_INGREDIENT = Ingredient.ofItems(Items.COD, Items.SALMON, Items.CHICKEN, Items.RABBIT);
+    }
+
     @Environment(EnvType.CLIENT)
     public Vec3d method_29919() {
         return new Vec3d(0.0D, 0.5F * this.getStandingEyeHeight(), this.getWidth() * 0.4F);
+    }
+
+    @Nullable
+    @Override
+    public PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
+        return null;
     }
 }
