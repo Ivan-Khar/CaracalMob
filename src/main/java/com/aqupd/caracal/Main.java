71ba81d58a04d2048656326de35a42dbf28cc967
AqUpd <47220198+AquUpd@users.noreply.github.com>
2021-05-25 21:36:38 +0300

    Натуральная генерация каракалов в мире!
    Теперь они генерируются в Саваннах!

--- src/main/java/com/aqupd/caracal/Main.java
+++ src/main/java/com/aqupd/caracal/Main.java
@@ -2,6 +2,7 @@ package com.aqupd.caracal;
 
 import net.fabricmc.api.ModInitializer;
 import com.aqupd.caracal.entities.CaracalEntity;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityDimensions;
@@ -10,6 +11,7 @@ import net.minecraft.entity.SpawnGroup;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.Biome;
 
 public class Main implements ModInitializer {
 
@@ -27,6 +29,11 @@ public class Main implements ModInitializer {
 
 		FabricDefaultAttributeRegistry.register(CARACAL, CaracalEntity.createcaracalAttributes());
 
-		//SpawnInit.init();
+		BiomeModifications.addSpawn(
+				selection -> selection.getBiome().getCategory() == Biome.Category.SAVANNA,
+				SpawnGroup.CREATURE,
+				CARACAL,
+				5, 1, 3 // weight/min group size/max group size
+		);
 	}
 }
--- src/main/java/com/aqupd/caracal/entities/CaracalEntity.java
+++ src/main/java/com/aqupd/caracal/entities/CaracalEntity.java
@@ -15,7 +15,6 @@ import net.minecraft.item.DyeItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvent;
@@ -37,7 +36,9 @@ public class CaracalEntity extends TameableEntity {
     protected void initGoals() {
         this.goalSelector.add(1, new SwimGoal(this));
         this.goalSelector.add(1, new SitGoal(this));
-        this.goalSelector.add(1, new EscapeDangerGoal(this, 1.4D));
+        
+        this.goalSelector.add(2, new FollowParentGoal(this, 1.4));
+        this.goalSelector.add(2, new EscapeDangerGoal(this, 1.4D));
         this.goalSelector.add(2, new AnimalMateGoal(this, 1.0D));
         this.goalSelector.add(2, new FollowOwnerGoal(this, 1.0D, 10.0F, 5.0F, false));
         this.goalSelector.add(3, new TemptGoal(this, 1.0D, false, TAMING_INGREDIENT));
@@ -48,9 +49,9 @@ public class CaracalEntity extends TameableEntity {
         this.goalSelector.add(8, new WanderAroundGoal(this, 0.5F));
         this.targetSelector.add(1, new TrackOwnerAttackerGoal(this));
         this.targetSelector.add(2, new AttackWithOwnerGoal(this));
-        this.targetSelector.add(3, new FollowTargetGoal<>(this, ChickenEntity.class, false));
-        this.targetSelector.add(4, new FollowTargetGoal<>(this, RabbitEntity.class, false));
-        this.targetSelector.add(5, new FollowTargetGoal<>(this, BatEntity.class, false));
+        this.targetSelector.add(3, new FollowTargetGoal<>(this, ChickenEntity.class, true));
+        this.targetSelector.add(4, new FollowTargetGoal<>(this, RabbitEntity.class, true));
+        this.targetSelector.add(5, new FollowTargetGoal<>(this, BatEntity.class, true));
 
     }
 
@@ -77,7 +78,7 @@ public class CaracalEntity extends TameableEntity {
         return PassiveEntity.createLivingAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 10D)
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.30000001192092896D)
-                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 64.0D)
+                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 20.0D)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 2.5D)
                 .add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, 0.5);
     }
