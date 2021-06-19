5d30b3c30426f9eb7f187cbd2f566472c448b840
AqUpd <47220198+AquUpd@users.noreply.github.com>
2021-06-19 14:24:37 +0300

    Не могу переделать модель на 1.17

--- src/main/java/com/aqupd/caracal/CaracalMainClient.java
+++ src/main/java/com/aqupd/caracal/CaracalMainClient.java
@@ -5,14 +5,15 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
+import net.minecraft.client.render.entity.EntityRendererFactory;
 
 @Environment(EnvType.CLIENT)
 public class CaracalMainClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        EntityRendererRegistry.INSTANCE.register(CaracalMain.CARACAL, (dispatcher, context) -> {
-            return new CaracalEntityRenderer(dispatcher);
+        EntityRendererRegistry.INSTANCE.register(CaracalMain.CARACAL, (EntityRendererFactory.Context context) -> {
+            return new CaracalEntityRenderer(context);
         });
     }
 }
