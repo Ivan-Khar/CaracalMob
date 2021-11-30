2ede983a25c9b76a5374b3e703ef843ae7f1210d
AqUpd <aquupd@ya.ru>
2021-11-30 21:41:34 +0300

    Update my log class

--- src/main/java/com/aqupd/caracal/utils/AqLogger.java
+++ src/main/java/com/aqupd/caracal/utils/AqLogger.java
@@ -5,13 +5,18 @@ import org.apache.logging.log4j.Logger;
 
 public class AqLogger {
     private static final Logger LOGGER = LogManager.getLogger();
+    private static String logprefix = "[AqUpd's mobs] ";
 
     public static void logError(String error) {
-        LOGGER.error("[AqUpd's Mobs] " + error);
+        LOGGER.error(logprefix + error);
+    }
+
+    public static void logWarn(String warn) {
+        LOGGER.warn(logprefix + warn);
     }
 
     public static void logInfo(String info) {
-        LOGGER.info("[AqUpd's Mobs] " + info);
+        LOGGER.info(logprefix + info);
     }
 
 }
