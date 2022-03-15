72948f01f12ba13c76d73f52a4de821ffda8af0d
AqUpd <aquupd@ya.ru>
2022-03-15 16:28:45 +0300

    update 1.18.1 to 1.18.2

--- src/main/java/com/aqupd/aqcaracal/Caracal.java
+++ src/main/java/com/aqupd/aqcaracal/Caracal.java
@@ -2,10 +2,7 @@ package com.aqupd.aqcaracal;
 
 import com.aqupd.aqcaracal.setup.Registration;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Caracal.MODID)
