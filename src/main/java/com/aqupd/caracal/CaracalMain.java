de12cf785721ccb1c9315bb1508c103036febf34
Ivan Kh <47220198+Ivan-Khar@users.noreply.github.com>
2023-07-02 06:56:54 +0300

    You can now configure caracal spawn biomes via datapack

--- src/main/java/com/aqupd/caracal/CaracalMain.java
+++ src/main/java/com/aqupd/caracal/CaracalMain.java
@@ -5,19 +5,20 @@ import com.aqupd.caracal.setup.CaracalSounds;
 import com.aqupd.caracal.utils.AqConfig;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SpawnEggItem;
-import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.Heightmap;
 
 import static com.aqupd.caracal.utils.AqLogger.logInfo;
@@ -39,21 +40,18 @@ public class CaracalMain implements ModInitializer {
     15592688,
     new FabricItemSettings().fireproof().maxCount(64));
 
+  public static final TagKey<Biome> CARACAL_SPAWN_BIOMES = TagKey.create(Registries.BIOME, new ResourceLocation("aqupd", "caracal_spawning_biomes"));
+
   @SuppressWarnings("DataFlowIssue")
   @Override
   public void onInitialize() {
-    //ServerWorldEvents.LOAD.register((server, world) -> AqDebug.INSTANCE.startDebug(AqConfig.INSTANCE.getBooleanProperty("debug")));
     CaracalSounds.init();
-
     Registry.register(BuiltInRegistries.ITEM, new ResourceLocation("aqupd", "caracal_spawn_egg"), CARACAL_SPAWN_EGG);
     FabricDefaultAttributeRegistry.register(CARACAL, CaracalEntity.createCaracalAttributes());
 
     BiomeModifications.addSpawn(
-      //TODO: Make it choose biomes by tag
-      BiomeSelectors.includeByKey(Biomes.SAVANNA),
-      MobCategory.CREATURE,
-      CaracalMain.CARACAL,
-      weight, mingroup, maxgroup
+      biomeSelectionContext -> biomeSelectionContext.hasTag(CARACAL_SPAWN_BIOMES),
+      MobCategory.CREATURE, CaracalMain.CARACAL, weight, mingroup, maxgroup
     );
 
     SpawnPlacements.register(
